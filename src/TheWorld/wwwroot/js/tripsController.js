036c636763d4c51ac646a581213af65261fc34b3
mslugx <rlvi_2001@yahoo.com>
2016-05-05 23:47:23 -0700

    angular added.

---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,38 @@
+﻿(function () {
+
+    "use strict";
+    angular.module("app-trips")
+    .controller("tripsController", tripsController);
+
+
+    function tripsController($http) {
+        var vm = this;
+        vm.trips = [];
+
+        vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                angular.copy(response.data, vm.trips);
+                
+
+            }, function (error) {
+                vm.errorMessage = "Fail to load" + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            })
+
+
+        vm.addTrip = function () {
+            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            //vm.newTrip.name = "";
+
+            vm.newTrip = {};
+        };
+    }
+})();
